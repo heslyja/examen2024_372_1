44ea2bc81aff8754b9e30c33f88abf656ad177b1
heslyja <158237057+heslyja@users.noreply.github.com>
2024-04-02 21:05:41 -0700

    Add files via upload

---
+++ create.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+rm -rf webapp
+mkdir -p webapp/{css,js,docs/{text,python},img}
+
+cd webapp
+
+for file in index.html style.css app.js main.py
+do
+    touch ./"${file}"
+done
+
+curl -o meme.jpg "https://pm1.aminoapps.com/7021/417429318f6b29b6bc737fb95ba1ddafc9ba2decr1-720-642v2_hq.jpg"
+
+read -p "Introduce la cantidad de archivos a crear:" numfiles
+for x in $( seq 1 $numfiles )
+do
+    echo "Archivo ${x}" > "file${x}.txt"
+done
+
+mv style.css css
+mv file?.txt docs/text
+mv main.py docs/python
+mv app.js js
+mv meme.jpg img
